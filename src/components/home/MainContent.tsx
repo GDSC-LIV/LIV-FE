fee496a9939122ef6250ff93368991bce7f98ac0
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-07-23 22:43:17 +0900

    feat:게시물 조회 api 연결

--- src/components/home/MainContent.tsx
+++ src/components/home/MainContent.tsx
@@ -1,77 +1,59 @@
-import React, { useState, useRef } from 'react';
-import Box from '@mui/material/Box';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import Tab from '@mui/material/Tab';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import Modal from '@mui/material/Modal';
-import PostModal from '../home/PostModal';
-import ProfileModal from '../home/ProfileModal';
-import Avatar from '@mui/material/Avatar';
-import { format } from 'date-fns';
-import { IconButton } from '@mui/material';
-import { FaRegComment, FaRegBookmark } from 'react-icons/fa';
-import CloseIcon from '@mui/icons-material/Close';
-
-const mockData = [
-  {
-    id: 1,
-    username: 'username11',
-    role: 'Backend developer',
-    projectTitle: '웹 프로젝트',
-    recruitmentField: '프론트엔드, 백엔드, 디자이너',
-    online: '온라인',
-    projectPeriod: '2023-06-01 - 2023-12-31',
-    projectDescription: '이번 프로젝트는...',
-    imageUrl: 'https://via.placeholder.com/200x100',
-    comments: [],
-    contactInfo: 'https://open.kakao.com/o/gxxxxxx', // Add contact info here
-  },
-  // Add more mock posts as needed
-  {
-    id: 2,
-    username: 'username11',
-    role: 'Backend developer',
-    projectTitle: '웹 프로젝트',
-    recruitmentField: '프론트엔드, 백엔드, 디자이너',
-    online: '온라인',
-    projectPeriod: '2023-06-01 - 2023-12-31',
-    projectDescription: '이번 프로젝트는...',
-    imageUrl: 'https://via.placeholder.com/200x100',
-    comments: [],
-    contactInfo: 'https://open.kakao.com/o/gxxxxxx', // Add contact info here
-  },
-  {
-    id: 3,
-    username: 'username11',
-    role: 'Backend developer',
-    projectTitle: '웹 프로젝트',
-    recruitmentField: '프론트엔드, 백엔드, 디자이너',
-    online: '온라인',
-    projectPeriod: '2023-06-01 - 2023-12-31',
-    projectDescription: '이번 프로젝트는...',
-    imageUrl: 'https://via.placeholder.com/200x100',
-    comments: [],
-    contactInfo: 'https://open.kakao.com/o/gxxxxxx', // Add contact info here
-  },
-];
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import Tab from "@mui/material/Tab";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import Modal from "@mui/material/Modal";
+import PostModal from "../home/PostModal";
+import ProfileModal from "../home/ProfileModal";
+import Avatar from "@mui/material/Avatar";
+import { format } from "date-fns";
+import { IconButton } from "@mui/material";
+import { FaRegComment, FaRegBookmark } from "react-icons/fa";
+import CloseIcon from "@mui/icons-material/Close";
 
 const MainContent: React.FC = () => {
-  const [selectedCategory, setSelectedCategory] = useState<string>('프로젝트 모집');
+  const [selectedCategory, setSelectedCategory] = useState<string>("RECRUIT");
+  const [posts, setPosts] = useState<any[]>([]);
   const [showMore, setShowMore] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [profileModalOpen, setProfileModalOpen] = useState(false);
-  const [profileModalPosition, setProfileModalPosition] = useState<{ top: number, left: number }>({ top: 0, left: 0 });
-  const [showComments, setShowComments] = useState<{ [key: number]: boolean }>({});
-  const [commentText, setCommentText] = useState<string>('');
-  const [comments, setComments] = useState<{ id: number, text: string, timestamp: Date }[]>([]);
+  const [profileModalPosition, setProfileModalPosition] = useState<{
+    top: number;
+    left: number;
+  }>({ top: 0, left: 0 });
+  const [showComments, setShowComments] = useState<{ [key: number]: boolean }>(
+    {}
+  );
+  const [commentText, setCommentText] = useState<string>("");
+  const [comments, setComments] = useState<
+    { id: number; text: string; timestamp: Date }[]
+  >([]);
   const [nextCommentId, setNextCommentId] = useState<number>(1);
   const [contactModalOpen, setContactModalOpen] = useState<boolean>(false);
-  const [contactInfo, setContactInfo] = useState<string>('');
+  const [contact, setcontact] = useState<string>("");
 
   const profileRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(`/posts?type=${selectedCategory}`);
+        if (response.data.isSuccess) {
+          setPosts(response.data.data);
+          console.log(response.data.data);
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, [selectedCategory]);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -82,13 +64,16 @@ const MainContent: React.FC = () => {
       const rect = profileRef.current.getBoundingClientRect();
       setProfileModalPosition({
         top: rect.top + window.scrollY,
-        left: rect.left + window.scrollX - 300, // Adjust left margin as needed
+        left: rect.left + window.scrollX - 300,
       });
     }
     setProfileModalOpen(!profileModalOpen);
   };
 
-  const handleCategoryChange = (event: React.SyntheticEvent, newValue: string) => {
+  const handleCategoryChange = (
+    event: React.SyntheticEvent,
+    newValue: string
+  ) => {
     setSelectedCategory(newValue);
   };
 
@@ -97,32 +82,34 @@ const MainContent: React.FC = () => {
   };
 
   const toggleComments = (postId: number) => {
-    setShowComments(prev => ({ ...prev, [postId]: !prev[postId] }));
+    setShowComments((prev) => ({ ...prev, [postId]: !prev[postId] }));
   };
 
   const handleCommentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCommentText(event.target.value);
   };
   const handleDeleteComment = (postId: number, commentId: number) => {
-    const updatedComments = comments.filter(comment => comment.id !== commentId);
+    const updatedComments = comments.filter(
+      (comment) => comment.id !== commentId
+    );
     setComments(updatedComments);
   };
 
   const handleSubmitComment = (postId: number) => {
-    if (commentText.trim() !== '') {
+    if (commentText.trim() !== "") {
       const newComment = {
         id: nextCommentId,
         text: commentText,
         timestamp: new Date(),
       };
       setComments([...comments, newComment]);
-      setCommentText('');
+      setCommentText("");
       setNextCommentId(nextCommentId + 1);
     }
   };
 
   const toggleContactModal = (info: string) => {
-    setContactInfo(info);
+    setcontact(info);
     setContactModalOpen(!contactModalOpen);
   };
 
@@ -152,17 +139,20 @@ const MainContent: React.FC = () => {
               onChange={handleCategoryChange}
               aria-label="lab API tabs example"
             >
-              <Tab label="Recruit" value="프로젝트 모집" />
-              <Tab label="promotion" value="광고나 홍보성 글" />
-              <Tab label="Recommend" value="개인 맞춤형 자동 피드보기" />
+              <Tab label="Recruit" value="RECRUIT" />
+              <Tab label="promotion" value="PROMOTION" />
+              <Tab label="Recommend" value="RECOMMEND" />
             </TabList>
           </Box>
         </TabContext>
       </Box>
 
-      {selectedCategory === "프로젝트 모집" &&
-        mockData.map((post) => (
-          <div key={post.id} className="bg-white p-4 shadow-sm rounded-lg mt-4">
+      {selectedCategory === "RECRUIT" &&
+        posts.map((post) => (
+          <div
+            key={post.post_id}
+            className="bg-white p-4 pr-8 pl-8 shadow-sm rounded-lg mt-4"
+          >
             <div className="flex justify-between items-center w-full">
               <div className="flex items-center space-x-4">
                 <img
@@ -178,23 +168,23 @@ const MainContent: React.FC = () => {
                   // position={profileModalPosition}
                 />
                 <div>
-                  <h3 className="font-semibold">{post.username} </h3>
+                  <h3 className="font-semibold">{post.name} </h3>
                   <p className="text-gray-600">{post.role}</p>
                 </div>
               </div>
               <Button
                 variant="outlined"
                 endIcon={<SendIcon />}
-                onClick={() => toggleContactModal(post.contactInfo)}
+                onClick={() => toggleContactModal(post.contact)}
               >
                 지원하기
               </Button>
             </div>
 
             <h4 className="font-semibold mt-4">프로젝트 주제</h4>
-            <p className="mt-2 text-gray-800">{post.projectTitle}</p>
+            <p className="mt-2 text-gray-800">{post.topic}</p>
             <h4 className="font-semibold mt-4">모집 분야</h4>
-            <p className="mt-2 text-gray-800">{post.recruitmentField}</p>
+            <p className="mt-2 text-gray-800">{post.field}</p>
 
             {!showMore && (
               <Button
@@ -210,11 +200,13 @@ const MainContent: React.FC = () => {
             {showMore && (
               <>
                 <h4 className="font-semibold mt-4">온/오프라인 유무</h4>
-                <p className="mt-2 text-gray-800">{post.online}</p>
+                <p className="mt-2 text-gray-800">
+                  {post.is_online ? "온라인" : "오프라인"}
+                </p>
                 <h4 className="font-semibold mt-4">프로젝트 기간</h4>
-                <p className="mt-2 text-gray-800">{post.projectPeriod}</p>
+                <p className="mt-2 text-gray-800">{post.duration}</p>
                 <h4 className="font-semibold mt-4">주요 내용</h4>
-                <p className="mt-2 text-gray-800">{post.projectDescription}</p>
+                <p className="mt-2 text-gray-800">{post.content}</p>
                 <Button
                   variant="text"
                   color="primary"
@@ -227,7 +219,7 @@ const MainContent: React.FC = () => {
             )}
 
             <img
-              src={post.imageUrl}
+              src="https://via.placeholder.com/200x100"
               alt="Project"
               className="h-60 mt-4 w-full rounded-lg"
             />
@@ -236,72 +228,74 @@ const MainContent: React.FC = () => {
               <div className="mt-4 flex justify-between">
                 <IconButton
                   sx={{ color: "gray" }}
-                  onClick={() => toggleComments(post.id)}
+                  onClick={() => toggleComments(post.post_id)}
                 >
                   <FaRegComment />
                 </IconButton>
-                <IconButton >
+                <IconButton>
                   <FaRegBookmark />
                 </IconButton>
               </div>
 
-              {showComments[post.id] && (
-  <div className="mt-4 border-t border-gray-200 pt-4">
-    <div className="flex"> {/* This div wraps the input field and button */}
-      <input
-        type="text"
-        value={commentText}
-        onChange={handleCommentChange}
-        placeholder="Write your comment..."
-        className="flex-grow border border-gray-300 rounded-lg px-4 py-2 mt-2"
-      />
-      <Button
-        variant="text"
-        onClick={() => handleSubmitComment(post.id)}
-        className="ml-2 mt-2"
-      >
-        <SendIcon style={{ color: 'black' }} />
-      </Button>
-    </div>
-    {comments.length > 0 && (
-  <div className="mt-4">
-    <h4 className="font-semibold">Comments</h4>
-    {comments.map((comment) => (
-      <div key={comment.id} className="flex items-start mt-2">
-        <Avatar
-          src="https://i.pinimg.com/originals/a7/ee/b8/a7eeb85a1d27390ebdf770f8cf31e434.jpg"
-          alt="Avatar"
-        />
-        <div className="flex-grow ml-3 flex justify-between">
-          <div>
-            <p className="text-sm text-gray-600">
-              {format(comment.timestamp, "yyyy-MM-dd HH:mm")}
-            </p>
-            <p className="mt-1">{comment.text}</p>
-          </div>
-          <IconButton
-            sx={{ color: "gray" }}
-            onClick={() => handleDeleteComment(post.id, comment.id)}
-          >
-            <CloseIcon />
-          </IconButton>
-        </div>
-      </div>
-    ))}
-  </div>
-)}
-
-  </div>
-)}
-
-
+              {showComments[post.post_id] && (
+                <div className="mt-4 border-t border-gray-200 pt-4">
+                  <div className="flex">
+                    <input
+                      type="text"
+                      value={commentText}
+                      onChange={handleCommentChange}
+                      placeholder="Write your comment..."
+                      className="flex-grow border border-gray-300 rounded-lg px-4 py-2 mt-2"
+                    />
+                    <Button
+                      variant="text"
+                      onClick={() => handleSubmitComment(post.post_id)}
+                      className="ml-2 mt-2"
+                    >
+                      <SendIcon style={{ color: "black" }} />
+                    </Button>
+                  </div>
+                  {comments.length > 0 && (
+                    <div className="mt-4">
+                      <h4 className="font-semibold">Comments</h4>
+                      {comments.map((comment) => (
+                        <div key={comment.id} className="flex items-start mt-2">
+                          <Avatar
+                            src="https://i.pinimg.com/originals/a7/ee/b8/a7eeb85a1d27390ebdf770f8cf31e434.jpg"
+                            alt="Avatar"
+                          />
+                          <div className="flex-grow ml-3 flex justify-between">
+                            <div>
+                              <p className="text-sm text-gray-600">
+                                {format(comment.timestamp, "yyyy-MM-dd HH:mm")}
+                              </p>
+                              <p className="mt-1">{comment.text}</p>
+                            </div>
+                            <IconButton
+                              sx={{ color: "gray" }}
+                              onClick={() =>
+                                handleDeleteComment(post.post_id, comment.id)
+                              }
+                            >
+                              <CloseIcon />
+                            </IconButton>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
           </div>
         ))}
 
-{selectedCategory === "광고나 홍보성 글" &&
-        mockData.map((post) => (
-          <div key={post.id} className="bg-white p-4 shadow-sm rounded-lg mt-4">
+      {selectedCategory === "PROMOTION" &&
+        posts.map((post) => (
+          <div
+            key={post.post_id}
+            className="bg-white p-4 shadow-sm rounded-lg mt-4"
+          >
             <div className="flex items-center space-x-4">
               <img
                 ref={profileRef}
@@ -316,16 +310,16 @@ const MainContent: React.FC = () => {
                 // position={profileModalPosition}
               />
               <div>
-                <h3 className="font-semibold">{post.username}</h3>
+                <h3 className="font-semibold">{post.name}</h3>
                 <p className="text-gray-600">{post.role}</p>
               </div>
             </div>
 
             <h4 className="font-semibold mt-4">프로젝트 주제</h4>
-            <p className="mt-2 text-gray-800">{post.projectTitle}</p>
+            <p className="mt-2 text-gray-800">{post.topic}</p>
 
             <img
-              src={post.imageUrl}
+              src="https://via.placeholder.com/200x100"
               alt="Project"
               className="h-60 mt-4 w-full rounded-lg"
             />
@@ -351,9 +345,9 @@ const MainContent: React.FC = () => {
           }}
         >
           <h2>Contact Information</h2>
-          <p>You can contact the project owner via the following link:</p>
-          <a href={contactInfo} target="_blank" rel="noopener noreferrer">
-            {contactInfo}
+          {/* <p>You can contact the project owner via the following link:</p> */}
+          <a href={contact} target="_blank" rel="noopener noreferrer">
+            {contact}
           </a>
         </Box>
       </Modal>
